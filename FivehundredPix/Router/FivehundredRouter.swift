cfb1273c80acd432b1ddd7687d2f862cfba6c90f
Jassadakorn Ketkaew <wizard-world@hotmail.co.th>
2017-02-02 12:26:23 +0700

    add router class

---
+++ FivehundredPix/Router/FivehundredRouter.swift
@@ -0,0 +1,13 @@
+//
+//  FivehundredRouter.swift
+//  FivehundredPix
+//
+//  Created by Jassadakorn Ketkaew on 2/2/2560 BE.
+//  Copyright © 2560 BE Jassadakorn Ketkaew. All rights reserved.
+//
+
+import UIKit
+
+class FivehundredRouter: NSObject {
+
+}
