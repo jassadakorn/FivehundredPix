dba28e530c7f0cb1239053269ddf00c72b2b3a9c
Jassadakorn Ketkaew <wizard-world@hotmail.co.th>
2017-02-04 17:07:20 +0700

    add photo list cell

---
+++ FivehundredPix/PhotoList/View/PhotoListCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoListCollectionViewCell.swift
+//  FivehundredPix
+//
+//  Created by Jassadakorn Ketkaew on 2/4/17.
+//  Copyright © 2017 Jassadakorn Ketkaew. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imgPhoto: UIImageView!
+}
